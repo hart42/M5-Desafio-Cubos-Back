8f7bdb27a262e3c88489181d81219bd300ddbc2c
Henrique Almeida <hart.aldtore@gmail.com>
2022-02-07 18:58:01 -0300

    Heroku Online

--- src/bancoDeDados/conexao.js
+++ src/bancoDeDados/conexao.js
@@ -1,10 +1,10 @@
 const knex = require('knex')({
   client: 'pg',
   connection: {
-      host: "ec2-34-206-148-196.compute-1.amazonaws.com",
-      user: "cowopnnkrfpxxz",
-      password: "e2c61bf5be6d046125461d59f719970e31fedf122079e998d08748ee2256d1ac",
-      database: "deihjakpag9mhs",
+      host: process.env.HDB_HOST,
+      user: process.env.HDB_USER,
+      password: process.env.HDB_PASSWORD,
+      database: process.env.HDB_DATABASE,
       port: 5432,
       ssl: {
         rejectUnauthorized: false
